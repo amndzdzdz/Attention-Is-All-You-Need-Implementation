325537ece5e83217ae349543a43b058a8b258bbb
amndzdzdz <amd9105@thi.de>
2024-08-25 09:01:12 +0200

    Added the Feed Forward Network

---
+++ modules/feed_forward.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super(FeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.fc(1)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
