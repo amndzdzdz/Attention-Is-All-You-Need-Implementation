a71c4213745a29c235d97fdcb7a8dc147d351f20
amndzdzdz <amd9105@thi.de>
2024-09-07 08:41:30 +0200

    added the Multi head attention module

--- modules/attention.py
+++ modules/attention.py
@@ -5,9 +5,13 @@ import numpy as np
 import math
 
 class ScaledDotProductAttention(nn.Module):
-    def __init__(self, dk, mask=False):
+    """
+    Implementation of the Scaled Dot Product Attention from the 
+    "Attention Is All You Need Paper"
+    """
+    def __init__(self, dim_k, mask=False):
         super(ScaledDotProductAttention, self).__init__()
-        self.dk = dk
+        self.dim_k = dim_k
         self.mask = mask
 
     def forward(self, query, keys, values):
@@ -21,18 +25,64 @@ class ScaledDotProductAttention(nn.Module):
             q_k_matmul = q_k_matmul + mask_matrix
 
         #scale the dot product with the inverse of the key dimension
-        q_k_matmul = torch.div(q_k_matmul, math.sqrt(self.dk))
+        q_k_matmul = torch.div(q_k_matmul, math.sqrt(self.dim_k))
 
         #q_k_matmul = q_k_matmul.apply_(lambda x: x / torch.sqrt(torch.tensor(self.dk)))
         q_k_softmax = F.softmax(q_k_matmul, dim=-1)
         x = torch.matmul(q_k_softmax, values)
+
         return x
 
+
+class MultiHeadAttention(nn.Module):
+    """
+    Implementation of the (Masked )Multi Head Attention Module from the
+    "Attention Is All You Need" Paper
+    """
+    def __init__(self, n_heads, dim_model, dim_q, dim_k, dim_v, mask=False):
+        super(MultiHeadAttention, self).__init__()
+        self.n_heads = n_heads
+        self.dim_model = dim_model
+        self.dims_at_hand = dim_model // n_heads
+
+        self.q_linear = nn.Linear(dim_q, dim_model)
+        self.k_linear = nn.Linear(dim_k, dim_model)
+        self.v_linear = nn.Linear(dim_v, dim_model)
+
+        self.attention = ScaledDotProductAttention(dim_model, mask=mask)
+
+        self.ln4 = nn.Linear(dim_model, dim_model)
+
+    def forward(self, query, key, value):
+        #Linearly Project the q, k and v first
+        query = self.q_linear(query)
+        key = self.k_linear(key)
+        value = self.v_linear(value)
+
+        seq_len = query.shape[1]
+        batch_size = query.shape[0]
+
+        #Split tensors across the n_heads and change place of seq_len and num_heads
+        query = query.view(batch_size, -1, self.n_heads, self.dims_at_hand).transpose(1, 2)
+        key = key.view(batch_size, -1, self.n_heads, self.dims_at_hand).transpose(1, 2)
+        value = value.view(batch_size, -1, self.n_heads, self.dims_at_hand).transpose(1, 2)
+
+        #calculate attention scores
+        attention_scores = self.attention(query, key, value)
+
+        #concat the attention scores across the n_heads 
+        attention_scores = attention_scores.transpose(-1, -2).contiguous().view(batch_size, -1, self.dim_model)
+
+        attention_scores = self.ln4(attention_scores)
+        
+        return attention_scores
+
 if __name__ == '__main__':
-    query = torch.rand(1, 2, 32, 16, device="cpu")
-    key = torch.rand(1, 2, 32, 16, device="cpu")
-    value = torch.rand(1, 2, 32, 16, device="cpu")
-    key_dim = query.shape[-1]
-    value_dim = value.shape[-1]
-    x = dot_prod(query, key, value)
+    query = torch.rand(1, 10, 512, device="cpu")
+    key = torch.rand(1, 10, 512, device="cpu")
+    value = torch.rand(1, 10, 512, device="cpu")
+    dim_q = query.shape[-1]
+    dim_v = value.shape[-1]
+    attention = MultiHeadAttention(8,dim_q, dim_q, dim_q, dim_v)
+    x = attention(query, key, value)
     print(x)
