a05d452f0d1b485fc623768c9cda3a27df70f96b
amndzdzdz <amd9105@thi.de>
2024-09-07 11:45:01 +0200

    removed feed forward from add&norm block

---
+++ modules/add_and_norm.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+from feed_forward import FeedForward
+
+class AddAndNorm(nn.Module):
+    """
+    Skip Connection Block + Layer Normalization
+    """
+    def __init__(self, dim):
+        super(AddAndNorm, self).__init__()
+        self.norm = nn.LayerNorm(dim)
+        
+    def forward(self, identity, x):
+        out = identity + x
+        out = self.norm(out)
+        return out
