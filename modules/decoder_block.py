555f8e3395223ba18e5329ae45a195940b3a9bd4
amndzdzdz <amd9105@thi.de>
2024-09-08 20:09:43 +0200

    added some changes

--- modules/decoder_block.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-class decoder_block(nn.Module):
-    def __init__(self):
-        super(decoder_block, self).__init__()
-
-    def forward(self, x):
-        return x
--- modules/encoder_block.py
+++
@@ -1,8 +0,0 @@
-import torch.nn as nn
-
-class encoder_block(nn.Module):
-    def __init__(self):
-        super(encoder_block, self).__init__()
-
-    def forward(self, x):
-        return x
