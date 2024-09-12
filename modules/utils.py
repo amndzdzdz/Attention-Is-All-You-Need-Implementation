d827d01852e86129a02647fab723a078cecffef4
amndzdzdz <amd9105@thi.de>
2024-09-12 20:06:11 +0200

    moved function into already existing utils file

--- modules/utils.py
+++ modules/utils.py
@@ -9,6 +9,25 @@ def preprocess_input(input_sequence, vocab_dict):
 
     return torch.LongTensor(output)
 
+def create_vocabularies(filepath):
+    en_vocabulary = {}
+    ar_vocabulary = {}
+    with open(filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            en, ar = line.replace("\n", "").split('\t')
+            
+            if en in en_vocabulary.keys():
+                en_vocabulary[en] += 1
+            else:
+                en_vocabulary[en] = 1
+            
+            if ar in ar_vocabulary.keys():
+                ar_vocabulary[ar] += 1
+            else:
+                ar_vocabulary[ar] = 1
+    
+    return en_vocabulary, ar_vocabulary
+
 
 if __name__ == '__main__':
 
